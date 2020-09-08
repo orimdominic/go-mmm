41ce69ba7aa06796ad5863df5670fc6a38406d4a
Orim Dominic <orimdominic@gmail.com>
2020-09-08 09:05:19 +0100

    feat: fixes #5

--- mean_test.go
+++ mean_test.go
@@ -5,15 +5,15 @@ import (
 )
 
 type testMean struct {
-	list   []float64
-	mean   float64
+	list []float64
+	mean float64
 }
 
 func TestMean(t *testing.T) {
 	tests := []testMean{
-		testMean{list: []float64{50, 12, 13, 12}, mean: 21.75,},
-		testMean{list: []float64{50, 52, 51, 53, 50}, mean: 51.2, },
-		testMean{list: []float64{10, 20, 40, 25, 30, 31}, mean: 26,},
+		testMean{list: []float64{50, 12, 13, 12}, mean: 21.75},
+		testMean{list: []float64{50, 52, 51, 53, 50}, mean: 51.2},
+		testMean{list: []float64{10, 20, 40, 25, 30, 31}, mean: 26},
 	}
 	for _, test := range tests {
 		got := mean(test.list)
