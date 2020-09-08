99f3228b7030c9380e6135d516940c4530cb1adf
Orim Dominic <orimdominic@gmail.com>
2020-09-08 05:51:17 +0100

    feat: get file

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func logErr(err error) {
+	fmt.Errorf(err.Error())
+}
+
+func getFile(path string) *os.File {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	return f
+}
+
+func getNums(f *os.File) (nums []float64) {
+	defer f.Close()
+	scnr := bufio.NewScanner(f)
+	for scnr.Scan() {
+		num, err := strconv.ParseFloat(scnr.Text(), 64)
+		if err != nil {
+			continue
+		}
+		nums = append(nums, num)
+	}
+	return
+}
+
+func mean(nums []float64) float64 {
+
+}
+
+func median(nums []float64) float64 {
+	return 0.00
+}
+
+func mode(nums []float64) float64 {
+	return 0.00
+}
+
+func main() {
+	file := getFile("data.txt")
+	nums := getNums(file)
+}
---
+++ main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMean(t *testing.T) {
+
+}
